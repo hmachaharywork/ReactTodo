af45517a8d6db0f1e8e49d0519e1a4bbede99156
am <hmachahary.work@gmail.com>
2017-02-17 04:59:41 +0530

    AddTodo added and tested

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,5 +1,6 @@
 var React = require('react');
 var TodoList = require('TodoList');
+var AddTodo = require('AddTodo');
 
 var TodoApp = React.createClass({
   getInitialState: function() {
@@ -20,11 +21,15 @@ var TodoApp = React.createClass({
       ]
     };
   },
+  handleAddTodo: function(newTodo) {
+    alert('new todo: ' + newTodo);
+  },
   render: function() {
     var {todos} = this.state;
     return(
       <div>
         <TodoList todos={todos} />
+        <AddTodo onAddTodo={this.handleAddTodo} />
       </div>
     );
   }
