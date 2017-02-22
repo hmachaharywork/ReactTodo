65afdd0e024dfd3e54bf346b2a9ecce093be575d
am <hmachahary.work@gmail.com>
2017-02-22 21:15:36 +0530

    Search feature wired up

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -19,5 +19,33 @@ module.exports = {
 
     return $.isArray(todos) ? todos : [];
 
+  },
+
+  filterTodos: function(todos, showCompleted, searchText) {
+    var filteredTodos = todos;
+
+    //Show all todos when showCompleted is true
+    filteredTodos = filteredTodos.filter((todo) => {
+      return !todo.completed || showCompleted;
+    });
+
+    //Filter by searchText
+    filteredTodos = filteredTodos.filter((todo) => {
+      var todoText = todo.text.toLowerCase();
+      return searchText.length === 0 || todoText.indexOf(searchText) > -1;
+    });
+
+
+    //Filter non completed todos to show first.
+    filteredTodos.sort((a, b) => {
+      if(!a.completed && b.completed) {
+        return -1;
+      }else if(a.completed && !b.completed) {
+        return 1;
+      }else {
+        return 0;
+      }
+    });
+    return filteredTodos;
   }
 };
