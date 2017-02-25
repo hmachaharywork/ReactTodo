27e8a501e2e6d099db3fade63a21cbcccc409410
am <hmachahary.work@gmail.com>
2017-02-25 16:32:02 +0530

    Actions generator created

---
+++ app/tests/actions/actions.test.jsx
@@ -0,0 +1,48 @@
+var expect = require('expect');
+
+var actions = require('actions');
+
+describe('Actions', () => {
+  it('should generate set search text action', () => {
+    var action = {
+      type: 'SET_SEARCH_TEXT',
+      searchText: 'Some text'
+    };
+
+    var res = actions.setSearchText(action.searchText);
+
+    expect(res).toEqual(action);
+  });
+
+  it('should generate add todo action', () => {
+    var action = {
+      type: 'ADD_TODO',
+      text: 'Thing to do'
+    };
+
+    var res = actions.addTodo(action.text);
+
+    expect(res).toEqual(action);
+  });
+
+  it('should generate toggleShowCompleted action', () => {
+    var action = {
+      type: 'TOGGLE_SHOW_COMPLETED'
+    };
+
+    var res = actions.toggleShowCompleted();
+
+    expect(res).toEqual(action);
+  });
+
+  it('should generate toggleTodo action', () => {
+    var action = {
+      type: 'TOGGLE_TODO',
+      id: 1
+    };
+
+    var res = actions.toggleTodo(action.id);
+
+    expect(res).toEqual(action);
+  });
+});
